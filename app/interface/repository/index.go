07271ddbd28a38263f80d5e19ed7c960153438cf
RaviAzzuraPutra <ravi.azzura74@gmail.com>
2026-02-26 14:20:33 +0700

    repository

---
+++ app/interface/repository/index.go
@@ -0,0 +1,11 @@
+package repository
+
+import "coba-firebase-golang/app/database/model"
+
+type Member_Repository_Interface interface {
+	Create(member *model.Member) error
+	GetAll() ([]model.Member, error)
+	GetById(ID string) (*model.Member, error)
+	Update(ID string, member *model.Member) error
+	Delete(ID string) error
+}
---
+++ app/repository/index.go
@@ -0,0 +1,93 @@
+package repository
+
+import (
+	"coba-firebase-golang/app/database/connect"
+	"coba-firebase-golang/app/database/model"
+	"context"
+
+	"cloud.google.com/go/firestore"
+)
+
+type Member_Repository struct {
+	DB *firestore.Client
+}
+
+func NewMemberRepositoryRegistry() *Member_Repository {
+	return &Member_Repository{
+		DB: connect.FirestoreClient,
+	}
+}
+
+func (repo *Member_Repository) Create(member *model.Member) error {
+
+	ctx := context.Background()
+
+	_, errCreate := repo.DB.Collection("members").Doc(member.ID).Set(ctx, member)
+
+	return errCreate
+
+}
+
+func (repo *Member_Repository) GetAll() ([]model.Member, error) {
+
+	ctx := context.Background()
+
+	iter := repo.DB.Collection("members").Documents(ctx)
+
+	var members []model.Member
+
+	for {
+		doc, err := iter.Next()
+
+		if err != nil {
+			break
+		}
+
+		var member model.Member
+
+		doc.DataTo(&member)
+
+		members = append(members, member)
+	}
+
+	return members, nil
+
+}
+
+func (repo *Member_Repository) GetById(ID string) (*model.Member, error) {
+
+	ctx := context.Background()
+
+	doc, err := repo.DB.Collection("members").Doc(ID).Get(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var member model.Member
+
+	doc.DataTo(&member)
+
+	return &member, nil
+
+}
+
+func (repo *Member_Repository) Update(ID string, member *model.Member) error {
+
+	ctx := context.Background()
+
+	_, err := repo.DB.Collection("members").Doc(ID).Set(ctx, member)
+
+	return err
+
+}
+
+func (repo *Member_Repository) Delete(ID string) error {
+
+	ctx := context.Background()
+
+	_, err := repo.DB.Collection("members").Doc(ID).Delete(ctx)
+
+	return err
+
+}
